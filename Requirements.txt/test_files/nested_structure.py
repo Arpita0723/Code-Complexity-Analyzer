68ed609b066148b8e0f2bc21b12d3655c73b5312
Arpita0723 <arpitasingh1jan2007@email.com>
2025-07-01 14:39:32 +0530

    Fix: removed py files from requirements.txt

--- Requirements.txt/test_files/nested_structure.py
+++
@@ -1,9 +0,0 @@
-def analyze(num):
-    if num > 0:
-        if num % 2 == 0:
-            for i in range(num):
-                print(i)
-        else:
-            print("Odd positive")
-    else:
-        print("Negative")
